2291dce0e702dc6f6f567f415c37387f1655481f
EunsilSon <eunsil1023@gmail.com>
2023-03-03 22:05:25 +0900

    Fix: [sort] 삽입 정렬 코드 오류 수정

--- sort/Insertion.java
+++ sort/Insertion.java
@@ -2,30 +2,28 @@ package sort;
 
 public class Insertion {
     public static void main(String[] args) {
-        int[] arr = {3, 1, 9, 5, 7};
+        int[] arr = {4, 2, 10, 26, 7, 1};
 
-        /*
-         * 기본 구현
-         */
+        // 기본 구현
         for (int i = 1; i < arr.length; i++) {
             for (int j = i-1; j >= 0; j--) {
                 if (arr[j] > arr[i]) {
-                    // swap
                     int temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
+
+                    i = j;
                 }
             }
         }
 
-        /*
-        * 최적화 구현 (앞 원소들이 정렬된 경우)
-        */
+        // 최적화 구현 (앞 원소들이 정렬된 경우)
         for (int i = 1; i < arr.length; i++) {
             int j = i-1;
 
             /**
-             * 조건: 타겟 앞의 원소의 idx가 0보다 크거나 같다 && 원소가 타겟보다 크다
+             * 1. 타겟 앞의 원소의 idx가 0보다 크거나 같고
+             * 2. 원소가 타겟보다 클 때
              * = 앞의 원소들 중 타겟보다 크면 swap하라
              */
             while(j >= 0 && arr[j] > arr[i]) {
@@ -33,6 +31,7 @@ public class Insertion {
                 arr[i] = arr[j];
                 arr[j] = temp;
 
+                i = j;
                 j--;
             }
         }
