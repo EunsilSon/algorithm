7afd130138ac2dabe33ee5033b7f2c85b10aa76d
EunsilSon <eunsil1023@gmail.com>
2023-03-03 21:09:36 +0900

    sort / 퀵 정렬 코드 변경 (middle pivot)

--- sort/Quick.java
+++ sort/Quick.java
@@ -2,41 +2,47 @@ package sort;
 
 public class Quick {
     public static void main(String[] args) {
-        int[] arr = new int[]{7, 2, 5, 1, 3, 4};
+        int[] arr = new int[]{26, 10, 35, 19, 7, 3, 12};
         Quick quick = new Quick();
-        quick.quickSort(arr, 0, arr.length-1);
+        quick.quickSortWithMiddlePivot(arr, 0, arr.length-1);
 
         for (int n : arr) {
             System.out.printf("%d ", n);
         }
     }
 
-    public void quickSort(int arr[], int left, int right) {
-        if (left < right) {
-            int pivot = partition(arr, left, right);
-
-            quickSort(arr, left, pivot-1); // 분할된 왼쪽 배열
-            quickSort(arr, pivot+1, right); // 분할된 오른쪽 배열
+    public void quickSortWithMiddlePivot(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
         }
-    }
 
-    public int partition(int arr[], int left, int right) {
-        int pivot = arr[right];
-        int i = (left - 1);
+        int pivot = arr[(left + right) / 2];
+        int i = left;
+        int j = right;
 
-        for (int j = left; j <= right-1; j++) {
-            if (arr[j] <= pivot) {
+        while (i < j) {
+            while (arr[i] < pivot) {
                 i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-
             }
-        }
-        int temp = arr[i+1];
-        arr[i+1] = arr[right];
-        arr[right] = temp;
 
-        return (i + 1); // pivot의 위치 반환
+            while (arr[j] > pivot) {
+                j--;
+            }
+
+            if (i >= j) {
+                break;
+            }
+
+            if (arr[i] == pivot && arr[j] == pivot) {
+                i++;
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        quickSortWithMiddlePivot(arr, left, i-1);
+        quickSortWithMiddlePivot(arr, i+1, right);
     }
 }
