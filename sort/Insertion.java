26c6a6c73dbd0c39546794f91f54659c34c7687b
EunsilSon <eunsil1023@gmail.com>
2023-03-05 23:16:03 +0900

    Fix : sort / 삽입 정렬 코드 수정 - 불필요한 반복을 도는 코드 삭제 - 배열의 길이 만큼만 반복하도록 수정

--- sort/Insertion.java
+++ sort/Insertion.java
@@ -4,36 +4,14 @@ public class Insertion {
     public static void main(String[] args) {
         int[] arr = {4, 2, 10, 26, 7, 1};
 
-        // 기본 구현
         for (int i = 1; i < arr.length; i++) {
-            for (int j = i-1; j >= 0; j--) {
-                if (arr[j] > arr[i]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+            int target = arr[i];
 
-                    i = j;
-                }
+            int j;
+            for (j = i-1; j >= 0 && arr[j] > target; j--) { // 앞의 원소가 타겟보다 작으면 멈춘다.
+                arr[j+1] = arr[j];
             }
-        }
-
-        // 최적화 구현 (앞 원소들이 정렬된 경우)
-        for (int i = 1; i < arr.length; i++) {
-            int j = i-1;
-
-            /**
-             * 1. 타겟 앞의 원소의 idx가 0보다 크거나 같고
-             * 2. 원소가 타겟보다 클 때
-             * = 앞의 원소들 중 타겟보다 크면 swap하라
-             */
-            while(j >= 0 && arr[j] > arr[i]) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-
-                i = j;
-                j--;
-            }
+            arr[j+1] = target;
         }
 
         for (int n : arr) {
